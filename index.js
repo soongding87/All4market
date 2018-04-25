825279be56989fb1b38fcf673cf6e8b97e8336fb
Taehoon Daniel Kang <kangtaehoon87@gmail.com>
2018-04-25 16:44:18 -0700

    commit

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ bye: 'buddy' });
 });
+
 
 const PORT = process.env.PORT || 5000 ;
 app.listen(PORT);
