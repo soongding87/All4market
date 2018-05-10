da3872cdd86251b3bb1fc62216ccb5fa6f6f75b8
Taehoon Daniel Kang <kangtaehoon87@gmail.com>
2018-05-10 16:24:20 -0700

    install redux-thunk and make navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
 
-const Header = () => <h2>Header </h2>;
+
 const DashBoard = () => <h2>Dashboard </h2>;
-const SurveyNew = () => <h2>surveyNEw </h2>;
+const SurveyNew = () => <h2>surveyNew </h2>;
 const Landing = () => <h2>Landing </h2>;
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <BrowserRouter>
         <div>
-          <Route path="/" component={Landing} />
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={DashBoard} />
+          <Route path="/surveys/new" component={SurveyNew} />
         </div>
       </BrowserRouter>
     </div>
